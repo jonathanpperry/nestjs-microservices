45cf8ce164dde1377959628fa3a790c3d66edfed
Jon Perry <jonperry113@gmail.com>
2025-04-05 11:45:52 -0400

    Validated request body

--- apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
+++ apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCoordinatesDTO } from './dto/create-coordinates.dto';
 
 @Controller('rider-coordinates')
 export class RiderCoordinatesController {
@@ -6,4 +7,13 @@ export class RiderCoordinatesController {
     getRiderCoordinates() {
         return "Hello I am from rider coordinates"
     }
+
+    @Post()
+    saveRiderCoordinates(
+        @Body()
+        createCoordinateDTO: CreateCoordinatesDTO
+    ) {
+        return createCoordinateDTO
+    }
+
 }
